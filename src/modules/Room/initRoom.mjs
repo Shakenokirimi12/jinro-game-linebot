e89e744e71d534c0fa31151c20cfa21f2ae27085
shakenokirimi12 <ken20051205@gmail.com>
2025-02-03 22:31:31 +0900

    Alpha版のBotに関する注意文を削除

--- src/modules/Room/initRoom.mjs
+++ src/modules/Room/initRoom.mjs
@@ -31,7 +31,6 @@ export async function initRoom(data, request, env, BOT_URL) {
                         { "type": "text", "text": "ルームを作成します。\r\nあなたのルームコードは" },
                         { "type": "text", "text": roomCode },
                         { "type": "text", "text": `です。\r\n参加する方は、URLから友達登録の上、以下のボタンを押してください。${BOT_URL}` },
-                        { "type": "text", "text": "現在このBotはAlpha版です。占いや騎士の守りなどが行えません。対応をお待ちください。" },
                         {
                             "type": "flex",
                             "altText": "starter",
